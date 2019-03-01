ab6d72a7c4ce4bcbccc51d9080778e92842a5122
kshell <kshell@aliyun.com>
2019-03-01 11:00:04 +0800

    MockMvc打印返回结果

--- spring-boot-helloworld/src/test/java/cn/kshell/SpringBootHelloworldApplicationTests.java
+++ spring-boot-helloworld/src/test/java/cn/kshell/SpringBootHelloworldApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -24,14 +25,21 @@ public class SpringBootHelloworldApplicationTests {
     private MockMvc mockMvc;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
     }
 
     @Test
     public void testHello() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Hello World!")));
+    }
+
+    @Test
+    public void testHello1() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
     }
 }
