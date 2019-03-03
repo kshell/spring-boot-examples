7e9129d29acf62e5938cddeb11099dd5433fc1bb
kshell <kshell@aliyun.com>
2019-03-03 23:00:15 +0800

    spring-boot-freemarker init

---
+++ spring-boot-freemaker/src/main/java/cn/kshell/SpringBootFreemakerApplication.java
@@ -0,0 +1,13 @@
+package cn.kshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreemakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFreemakerApplication.class, args);
+    }
+
+}
