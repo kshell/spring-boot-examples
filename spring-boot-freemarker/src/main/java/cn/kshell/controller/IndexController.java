d193e14cdc0a39404def9eca081402c88075f32b
kshell <kshell@aliyun.com>
2019-03-04 19:16:26 +0800

    freemaker modify

--- spring-boot-freemarker/src/main/java/cn/kshell/controller/IndexController.java
+++ spring-boot-freemarker/src/main/java/cn/kshell/controller/IndexController.java
@@ -1,5 +1,7 @@
 package cn.kshell.controller;
 
+import cn.kshell.model.Product;
+import cn.kshell.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,16 @@ public class IndexController {
 
     @GetMapping("/index")
     public String index(Model model){
-        model.addAttribute("userName", "Kong Weihua");
+        model.addAttribute("userName", "孔维华");
         return "hello";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model){
+        User user = new User("kshell孔维华", "12345678" );
+        Product product = new Product("green mouse", "products/greenmouse.html");
+        model.addAttribute(user);
+        model.addAttribute(product);
+        return "user";
     }
 }
---
+++ spring-boot-freemarker/src/main/resources/templates/user.ftl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h3>Welcome ${user.name} ${user.password}</h3>
+<p>Our latest product:
+    <a href="${product.url}">${product.name}</a>!
+<hr/>
+${user.name}
+</body>
+</html>
