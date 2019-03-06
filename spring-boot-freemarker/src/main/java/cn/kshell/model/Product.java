298841aa73ac434779dc4aee459020b13356b7a2
kshell <kshell@aliyun.com>
2019-03-06 18:15:30 +0800

    使用lombok

--- spring-boot-freemarker/src/main/java/cn/kshell/model/Product.java
+++ spring-boot-freemarker/src/main/java/cn/kshell/model/Product.java
@@ -1,31 +1,13 @@
 package cn.kshell.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Product {
     private String name;
     private String url;
-
-    public Product(String name) {
-        this.name = name;
-    }
-
-    public Product(String name, String url) {
-        this.name = name;
-        this.url = url;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
--- spring-boot-freemarker/src/main/java/cn/kshell/model/User.java
+++ spring-boot-freemarker/src/main/java/cn/kshell/model/User.java
@@ -1,30 +1,13 @@
 package cn.kshell.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
     private String name;
     private String password;
-
-    public User() {
-    }
-
-    public User(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
