715c0138f334e9d87b19e11085217727bcc27818
kshell <kshell@aliyun.com>
2019-05-30 13:51:27 +0800

    删除gradle-wrapper

---
+++ spring-boot-jpa-scheduler/src/main/java/cn/kshell/Main.java
@@ -0,0 +1,64 @@
+package cn.kshell;
+
+/**
+ * @author kshell
+ * @version v1.0
+ * Date :2019-05-24
+ */
+
+@SuppressWarnings("all")
+public class Main {
+
+    public static void main(String[] args) {
+
+        org.json.jsonObje
+
+    }
+
+    static String jsonStr="{\n" +
+            "\t\"c1\": [{\n" +
+            "\t\t\"date\": 2018 - 04 - 30,\n" +
+            "\t\t\"lsh\": ,\n" +
+            "\t\t\"hostnum\": 157799,\n" +
+            "\t\t\"sum\": 111,\n" +
+            "\t\t\"amt\": 6,\n" +
+            "\t\t934.00\n" +
+            "\t}, {\n" +
+            "\t\t\"date\": 2018 - 04 - 30,\n" +
+            "\t\t\"lsh\": ,\n" +
+            "\t\t\"hostnum\": 111348,\n" +
+            "\t\t\"sum\": 222,\n" +
+            "\t\t\"amt\": 2,\n" +
+            "\t\t784,\n" +
+            "\t\t164.30\n" +
+            "\t}, ],\n" +
+            "\t\"c2\": [],\n" +
+            "\t\"c3\": [{\n" +
+            "\t\t\"adjdate\": 2018 - 04 - 22,\n" +
+            "\t\t\"summary\": OBSS002395298807GIRO2630aa94154b,\n" +
+            "\t\t\"adjamt\": 81,\n" +
+            "\t\t730.00\n" +
+            "\t}, {\n" +
+            "\t\t\"adjdate\": 2018 - 04 - 22,\n" +
+            "\t\t\"summary\": OBSS002395298800GIRO2630aa94154b,\n" +
+            "\t\t\"adjamt\": 30,\n" +
+            "\t\t684.78\n" +
+            "\t}, {\n" +
+            "\t\t\"adjdate\": 2018 - 04 - 22,\n" +
+            "\t\t\"summary\": OBSS002395298797GIRO2630aa94154b,\n" +
+            "\t\t\"adjamt\": 20,\n" +
+            "\t\t551.40\n" +
+            "\t}, {\n" +
+            "\t\t\"adjdate\": 2018 - 04 - 22,\n" +
+            "\t\t\"summary\": OBSS002395298784GIRO2630aa94154b,\n" +
+            "\t\t\"adjamt\": 360,\n" +
+            "\t\t000.00\n" +
+            "\t}, {\n" +
+            "\t\t\"adjdate\": 2018 - 04 - 22,\n" +
+            "\t\t\"summary\": OBSS002395298947GIRO2630aa94154b,\n" +
+            "\t\t\"adjamt\": 250,\n" +
+            "\t\t533.09\n" +
+            "\t}, ],\n" +
+            "\t\"c4\": []\n" +
+            "}";
+}
